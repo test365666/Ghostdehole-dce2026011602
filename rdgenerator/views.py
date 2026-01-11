c55a6c2dedc53907d76d834184bc11bdae1a028a
Ghostdehole <dihenggan@outlook.com>
2026-01-12 01:48:41 +0800

    Fix

--- rdgenerator/views.py
+++ rdgenerator/views.py
@@ -248,7 +248,7 @@ def generator_view(request):
             create_github_run(myuuid)
             response = requests.post(url, json=data, headers=headers)
             print(response)
-            if response.status_code == 204 or response.status_code == 200:
+            if 200 <= response.status_code < 300:
                 return render(request, 'waiting.html', {'filename':filename, 'uuid':myuuid, 'status':"Starting generator...please wait", 'platform':platform})
             else:
                 return JsonResponse({"error": "Something went wrong"})
